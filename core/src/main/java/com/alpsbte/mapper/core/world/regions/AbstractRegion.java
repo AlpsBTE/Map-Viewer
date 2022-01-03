5f2c98d13a888fbc4eb31f8a25026a3e46ede842
LordTuxn <r3tuxn@gmail.com>
2022-01-03 16:28:30 +0100

    add support for .mca region files

---
+++ core/src/main/java/com/alpsbte/mapper/core/world/regions/AbstractRegion.java
@@ -0,0 +1,63 @@
+package com.alpsbte.mapper.core.world.regions;
+
+import java.io.DataInputStream;
+import java.io.File;
+
+public abstract class AbstractRegion {
+
+    protected int[] offsets;
+
+    private final File file;
+
+    /**
+     * Starts reading the region file
+     * @param regionFile - path to region file
+     */
+    public AbstractRegion(File regionFile) {
+        this.file = regionFile;
+        if (regionFile.exists() && regionFile.canRead()) {
+            readRegion(regionFile);
+        }
+    }
+
+    /**
+     * The offset for a chunk (x, z) begins at byte value 4*(x+z*32).
+     * Since every region file begins with a 4 KB header that describes where chunks are stored, we have to
+     * multiply it by the chunk position. The chunk position is calculated by adding the x and z value and multiply
+     * it by 32, since every region files stores a group of 32x32 chunks.
+     */
+    protected abstract void readRegion(File regionFile);
+
+
+    /**
+     * Reads uncompressed region file and returns chunk data
+     * @param chunkX - relative chunk x
+     * @param chunkZ - relative chunk z
+     * @return chunk data
+     */
+    public abstract DataInputStream readChunkData(int chunkX, int chunkZ, File regionFile);
+
+
+    /**
+     * Checks if a given chunk is valid
+     * @param chunkX - relative chunk x
+     * @param chunkZ - relative chunk z
+     * @return true if chunk is valid (in region file)
+     */
+    protected abstract boolean outOfBounds(int chunkX, int chunkZ);
+
+
+    /**
+     * Returns chunk offset in region file
+     * {@link #readRegion(File)}}
+     *
+     * @param chunkX - relative chunk x
+     * @param chunkZ - relative chunk z
+     * @return chunk offset
+     */
+    protected abstract int getChunkOffset(int chunkX, int chunkZ);
+
+    public File getFile() {
+        return file;
+    }
+}
